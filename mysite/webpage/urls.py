e61b309ad758cdb82eb837856261dafe08703c8c
Olinka <olga.titzenthaler@gmail.com>
2024-02-10 00:17:47 +0100

    start app webpafe

---
+++ mysite/webpage/urls.py
@@ -0,0 +1,8 @@
+#MyWebPage/mysite/webpage/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
---
+++ mysite/webpage/views.py
@@ -0,0 +1,10 @@
+
+#MyWebPage/mysite/webpage/views.py
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at my webpage.")
